013cf2ecc20ed57f5dd4b4a7ca6a38fd65959465
HoustonMiles <mileshs1@g.cofc.edu>
2024-10-03 22:00:53 -0400

    Added creds

---
+++ src/creds.java
@@ -0,0 +1,5 @@
+public class creds {
+    public static String databaseid="jdbc:mysql://identileafdb.cfsskm8gg0nz.us-east-2.rds.amazonaws.com";
+    public static String dbpassword="identileaf";
+    public static String dbusername="admin";
+}
