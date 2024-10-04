acba3263c72957888c7f136680db814afc2e87d9
Jason Hardwick <jasonhardwick96@gmail.com>
2024-10-03 20:42:07 -0400

    Implement SQL connection

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        try (Connection con = DriverManager.getConnection(creds.databaseid, creds.dbusername, creds.dbpassword)) {
+
+            Statement stmt = con.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select scientificName, commonName, BarkType.type, LeafType.type, PlantType.type from mydb.Tree\n" +
+                    "inner join mydb.BarkType\n" +
+                    "on Tree.BarkType_ID = BarkType.ID\n" +
+                    "inner join mydb.LeafType\n" +
+                    "on Tree.LeafType_ID = LeafType.ID\n" +
+                    "inner join mydb.PlantType\n" +
+                    "on Tree.PlantType_ID = PlantType.ID");
+
+            while (rs.next()) {
+                System.out.println("Name: " + rs.getString("commonName") +
+                        "; Plant Type: " + rs.getString("PlantType.type") +
+                        "; Leaf Type: " + rs.getString("LeafType.type") +
+                        "; Bark Type: " + rs.getString("BarkType.type"));
+            }
         }
     }
 }
